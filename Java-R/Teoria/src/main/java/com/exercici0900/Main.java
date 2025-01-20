810ed76fcdeccc4078d3afd8f95adf0a043bd57c
Bermu <alexbermudezcarmona@gmail.com>
2025-01-20 18:28:07 +0100

    Empezando primera actividad de repaso

--- Java-R/Teoria/src/main/java/com/exercici0900/Main.java
+++ Java-R/Teoria/src/main/java/com/exercici0900/Main.java
@@ -1,10 +1,26 @@
 package com.exercici0900;
 
+import java.util.Scanner;
+
 public class Main {
+
+    // FUNCION DEL ANIGRAMA
+    public static boolean isAnagrama(String palabra1, String palabra2){
+        palabra1.trim().replaceAll("\\s+", "");
+
+        return false;
+        
+
+    }
     public static void main(String[] args) {
         
-        /* TODO: 
-           Fes aquí el codi de l'exercici
-        */
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Pon una palabra");
+        String palabra1Input = scanner.nextLine();
+
+        palabra1Input.trim();
+        String resultado = palabra1Input.replaceAll("\\s+", ""); // Para quitar los espacios en blanco entre palabras
+        System.out.println(resultado);
     }
 }
