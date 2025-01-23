3c030eb539ffb2f145a8f9585ac3f26631662b81
Bermu <alexbermudezcarmona@gmail.com>
2025-01-23 13:03:39 +0100

    .

--- Java-R/Teoria/src/main/java/com/exercici0000/Main.java
+++ Java-R/Teoria/src/main/java/com/exercici0000/Main.java
@@ -3,8 +3,143 @@ package com.exercici0000;
 public class Main {
     public static void main(String[] args) {
         
-        /* TODO: 
-           Fes aquí el codi de l'exercici
+        /* [APUNTES JAVA EXAMEN]
+
+            [INDICE DE TEMARIO EXAMEN JAVA]
+
+            [APUNTES DE JAVA ]
+
+            * DAM-PROGRAMACIO (EXERCICI0000)
+
+            [TABLA CON ARRAYLIST Y HASHMAP / DAM-PROGRAMACIO (EXERCICI0909)]
+
+            * INPUTS ESCRIBIENDO "TEXTO" = DAM-PROGRAMACIO (EXERCICI0909)
+            * ORDENAR POR NOMBRE = DAM-PROGRAMACIO (EXERCICI0909)
+            * ORDENAR POR APELLIDO = DAM-PROGRAMACIO (EXERCICI0909)
+            * ORDENAR POR TELEFONO = DAM-PROGRAMACIO (EXERCICI0909)
+            * ORDENAR TODO INVERTIDO = DAM-PROGRAMACIO (EXERCICI0909)
+            * FILTRAR = DAM-PROGRAMACIO (EXERCICI0909)
+            * CREAR USUARIO NUEVO = DAM-PROGRAMACIO (EXERCICI0909)
+            * BORRAR USUARIO = DAM-PROGRAMACIO (EXERCICI0909)
+            * GENERAR ID = DAM-PROGRAMACIO (EXERCICI0909)
+
+            [ EJERCICIOS LOGICOS ]
+
+            * CALCULAR TARIFA = DAM-PROGRAMACIO (EXERCICI0904)
+            * CALCULAR COSTE HABITACION (CON DESCUENTOS) = DAM-PROGRAMACIO (EXERCICI0903)
+
+            [ EJERCICIOS RANDOM ]
+
+            * TIRAR UNA MONEDA = DAM-PROGRAMACIO (EXERCICI0901)
+            --------------------------------------------------------------------------------------
+
+            [IMPORTS]
+
+            - (Scanner) = import java.util.Scanner; --> Scanner scanner = new Scanner(System.in);
+            - (HashMap) = import java.util.HashMap; -->
+            - (ArrayList) = import java.util.ArrayList;
+            - (HashSet) = import java.util.HashSet;
+            - (Set) = import java.util.Set;
+            - (Random) = import java.util.Random;
+            - (Local) = import java.util.Locale; --> Locale localeUS = Locale.US; || Locale localeES = Locale.forLanguageTag("es")
+            
+            - (JSONArray) = import org.json.JSONArray;
+            - (JSONObject) = import org.json.JSONObject;
+
+            - (Paths) = import java.nio.file.Paths;
+            - (LocalDate) = import java.time.LocalDate;
+            - (DateTimeFormatter) = import java.time.format.DateTimeFormatter;
+            - (Files) = import java.nio.file.Files;
+            - (FileWriter) = import java.io.FileWriter;
+            ---------------------------------------------------------------------------------------------------------------------------------------
+
+            [TEXTOS]
+
+            - Quitar espacios al principio y final = .trim();
+            - Pasar la cadena a minusculas = .toLowerCase();
+            - Convertir la primera letra en mayusculas = texto.substring(0, 1).toUpperCase() + texto.substring(1).toLowerCase(); || Ej: alex = Alex
+            - Longitud = string.length();
+            - Imprimir con decimales bien = System.out.printf(localeUS, "El valor de %.2f€ són %.2f$%n", euros, dollars);
+            - Formato para una tabla = System.out.printf("%-6d| %-15s| %-20s| %-10d%n", valor, valor, valor , valor)
+            ----------------------------------------------------------------------------------------------------------------------------------------
+
+            [VERIFICADORES]
+
+            - Si esta vacio = entrada.isEmpty();
+            - Si la entrada tiene espacio = entrada.contains(" ");
+            - Si tiene que tener unos numeros EXACTOS = entrada.matches("\\d{9}");
+            - Si la entrada tiene que estar entre unos valores (0 y 20) = (num >= 0 && num <= 20);
+            - Si la entrada que tiene NO EXISTE = (entrada == null);
+            - Comparar Si es una cosa U otra = ||
+            - Comparar Si una comple con la otra = &&
+            ----------------------------------------------------------------------------------------------------------------------------------------
+
+            [ITERAR UNA LISTA / ARRAY / ETC]
+
+            - Para json = for (int i = 0; i < valor.length(); i++)
+            - Para HashMap = for (int i = 0; i < valor.size(); i++)
+            ----------------------------------------------------------------------------------------------------------------------------------------
+
+            [JSON]
+
+            SET-UP para que lea el archivo .json =
+
+            String filePathPrestecs = "./src/main/json/prestecs.json";
+            String contentPrestecs = new String(Files.readAllBytes(Paths.get(filePathPrestecs)));
+
+            JSONArray jsonArrayPrestecs = new JSONArray(contentPrestecs);
+            
+            DAR FORMATO A LAS CLAVES DEL JSON
+
+            int idUsuari = usuari.getInt("idUsuari");
+            String nom = usuari.getString("nom");
+            String cognoms = usuari.getString("cognoms");
+            int telefon = usuari.getInt("telefon");
+            ------------------------------------------------------------------------------------------------------------------------------------------
+
+            [CREAR UN MENU]
+
+            int optMain = 0;
+
+            // MENÚ PRINCIPAL
+            do {
+                System.out.println("""
+                    -- Gestio de biblioteca --
+                    1. Llibres
+                    2. Usuaris
+                    3. Prestecs
+                    0. Sortir
+                    Escull una opcio: """);
+                if (scanner.hasNextInt()) {
+                    optMain = scanner.nextInt();
+
+                    switch (optMain) {
+                        case 1:
+                            limpiarPantalla();
+                            menuLibros(scanner);
+                            break;
+                        case 2:
+                            limpiarPantalla();
+                            menuUsuarios(scanner);
+                            break;
+                        case 3:
+                            limpiarPantalla();
+                            menuPrestecs(scanner);
+                            break;
+                        case 0:
+                            limpiarPantalla();
+                            break;
+                        default:
+                            System.out.println("Opcio no valida. Torna-ho a intentar.\n");
+                    }
+                } else {
+                    System.out.println("Si us plau, ingressa un número valid.\n ");
+                    scanner.next();
+                    optMain = -1;
+                }
+            } while (optMain != 0);
         */
     }
-}
+
+    }
+
--- Java-R/Teoria/src/main/java/com/exercici0909/Main.java
+++ Java-R/Teoria/src/main/java/com/exercici0909/Main.java
@@ -1,10 +1,344 @@
 package com.exercici0909;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
         
-        /* TODO: 
-           Fes aquí el codi de l'exercici
-        */
+        Scanner scanner = new Scanner (System.in);
+
+        // Lista que almacenara las filas de la tabla
+        ArrayList<HashMap<String,Object>> tabla = new ArrayList<>();
+        
+        // Creamos primera fila con HashMap
+        HashMap<String,Object> fila1 = new HashMap<>();
+        fila1.put("Nom", "Albert Einstein");
+        fila1.put("Pais" , "Alemanya/Suissa");
+        fila1.put("Disciplina" , "Fisica");
+        fila1.put("Nobel" , "Si");
+        fila1.put("Naixement", 1879);
+
+        // Creamos segunda fila con HashMap
+        HashMap<String,Object> fila2 = new HashMap<>();
+        fila2.put("Nom", "Marie Curie");
+        fila2.put("Pais" , "Polonia/França");
+        fila2.put("Disciplina" , "Fisica/Quimica");
+        fila2.put("Nobel" , "Si");
+        fila2.put("Naixement", 1867);
+
+        // Creamos tercera con HashMap
+        HashMap<String,Object> fila3 = new HashMap<>();
+        fila3.put("Nom", "Isaac Newton");
+        fila3.put("Pais" , "Anglaterra");
+        fila3.put("Disciplina" , "Física/Matematiques");
+        fila3.put("Nobel" , "No");
+        fila3.put("Naixement", 1643);
+
+        // Creamos cuarta fila con HashMap
+        HashMap<String,Object> fila4 = new HashMap<>();
+        fila4.put("Nom", "Charles Darwin");
+        fila4.put("Pais" , "Anglaterra");
+        fila4.put("Disciplina" , "Anglaterra");
+        fila4.put("Nobel" , "No");
+        fila4.put("Naixement", 1809);
+
+        // Creamos quinta fila con HashMap
+        HashMap<String,Object> fila5 = new HashMap<>();
+        fila5.put("Nom", "Alan Turing");
+        fila5.put("Pais" , "Anglaterra");
+        fila5.put("Disciplina" , "Matematiques/Infor.");
+        fila5.put("Nobel" , "No");
+        fila5.put("Naixement", 1912);
+
+        // Agregamos las filas a la Arraylist
+
+        tabla.add(fila1);
+        tabla.add(fila2);
+        tabla.add(fila3);
+        tabla.add(fila4);
+        tabla.add(fila5);
+
+        generaId(tabla); // IMPORTANTE PONERLO ARRIBA YA QUE LO LEE POR ORDEN DE LINEA
+
+        // Imprimir la tabla en buen formato
+        imprimirTabla(tabla);
+
+        menuTabla(scanner, tabla);
+
+        
     }
+
+    public static void imprimirTabla(ArrayList<HashMap<String, Object>> tabla){
+        //Imprimir cabecera
+        System.out.println("|  ID  | Nom               | Pais            | Disciplina        | Nobel  | Naixement|");
+        System.out.println("|------------------------------------------------------------------------------------|");
+
+        // Imprimir las filas
+        for (HashMap<String, Object> fila : tabla) {
+            Integer id = (Integer) fila.get("ID");
+            String nom = (String) fila.get("Nom");
+            String pais = (String) fila.get("Pais");
+            String disciplina = (String) fila.get("Disciplina");
+            String nobel = (String) fila.get("Nobel");
+            Integer naixement = (Integer) fila.get("Naixement");
+
+            // Imprimir la fila de datos (estructurado)
+            System.out.printf("| %-2d | %-17s | %-15s | %-19s | %-6s | %-9d|\n", id, nom,pais,disciplina,nobel,naixement);
+        }
+
+    }
+
+    public static void menuTabla(Scanner scanner, ArrayList<HashMap<String, Object>> tabla) {
+        String opt = "";
+
+        do { 
+            System.out.print("Que vols fer: ");
+            opt = scanner.nextLine().toLowerCase().trim();
+
+            if (opt.startsWith("afegir ")) {
+                crearPersonaje(opt.substring(7), tabla);
+            } else if (opt.startsWith("esborrar ")) {
+                borrarPersonaje(opt.substring(9).trim(), tabla);
+            } else if (opt.startsWith("filtrar ")) {
+                String[] parts = opt.substring(8).split(" ", 2);
+                if (parts.length == 2) {
+                    String columna = parts[0].trim();
+                    String valor = parts[1].trim().replaceAll("^\"|\"$", ""); // Elimina las comillas
+                    filtrar(columna, valor, tabla);
+                }
+            }
+            
+                switch (opt) {
+                    case "ordenar nom":
+                        mostrarTablaOrdenadaNombre(tabla);
+                        break;
+                    case "ordenar pais":
+                        mostrarTablaOrdenadaPais(tabla);
+                        break;
+                    case "ordenar disciplina":
+                        mostrarTablaOrdenadaDisciplina(tabla);
+                        break;
+                    case "ordenar naixement":
+                        mostrarTablaOrdenadaNaixement(tabla);
+                        break;
+                    case "ordenar nom inv":
+                        mostrarTablaOrdenadaNomInvertido(tabla);
+                        break;
+                    case "ordenar pais inv":
+                        mostrarTablaOrdenadaPaisInvertido(tabla);
+                        break;
+                    case "ordenar disciplina inv":
+                        mostrarTablaOrdenadaDisciplinaInvertido(tabla);
+                        break;
+                    case "ordenar naixement inv":
+                        mostrarTablaOrdenadaNaixementInvertido(tabla);
+                        break;
+                    case "ajuda":
+                        menuAyuda();
+                        break;
+                    case "sortir":
+                        System.out.println("Sortint...");
+                        System.exit(0);
+                    default:
+                        System.out.println("Opcio no valida.");
+                        break;
+                }
+
+                imprimirTabla(tabla);
+            
+        } while (true);
+    }
+
+    // Mostrar menu de ayuda
+    public static void menuAyuda() {
+        System.out.println("\n"+"""
+            --- COMANDES PER UTILITZAR ---
+            1. ordenar nom = (Ordena els noms alfabeticament)
+            2. ordenar pais = (Ordena els paisos alfabeticament)
+            3. ordenar disciplina = (Ordena la disciplines alfabeticament)
+            4. ordenar naixement = (Ordena els naixements numericament)
+            
+            === Si vols ordenar a la inversa ==
+            - Utilizta la mateixa comanda amb "inv" al final
+            Exemple: ordenar nom inv
+            ---------------------------------------------------------------
+                """);
+    }
+
+    // Crear nuevo personaje
+    public static void crearPersonaje(String dades, ArrayList<HashMap<String, Object>> tabla) {
+        String[] parts = dades.split(",");
+
+        if (parts.length != 5) {
+            System.out.println("Error: El format ha de ser Nom,Pais,Disciplina,Nobel,Naixement");
+            return;
+        }
+
+        try {
+            HashMap<String, Object> novaFila = new HashMap<>();
+
+            novaFila.put("Nom", capitalize(parts[0].trim()));
+            novaFila.put("Pais", capitalize(parts[1].trim()));
+            novaFila.put("Disciplina", capitalize(parts[2].trim()));
+            novaFila.put("Nobel", capitalize(parts[3].trim()));
+            novaFila.put("Naixement", Integer.parseInt(parts[4].trim()));
+
+            tabla.add(novaFila);
+            generaId(tabla);
+            System.out.println("Nou personatge afegit amb exit");
+
+        } catch (NumberFormatException e) {
+            System.out.println("Error: L'any de naixement ha de ser enter.");
+        }
+    }
+
+    // Funcion para volver la primera letra del string a mayusculas
+    public static String capitalize(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+
+        return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+    }
+
+    // Funcion eliminar personaje 
+    public static void borrarPersonaje(String nom, ArrayList<HashMap<String, Object>> tabla) {
+        boolean encontrado = false;
+
+        // Recorremos la tabla para buscar el personaje por su nombre
+        for (int i = 0; i < tabla.size(); i++) {
+            HashMap<String, Object> fila = tabla.get(i);
+            if (fila.get("Nom").toString().equalsIgnoreCase(nom.trim())) {
+                tabla.remove(i);
+                encontrado = true;
+                System.out.println("Personatge '" + nom + "' eliminat amb exit");
+                break;
+            }
+        }
+
+        if (!encontrado) {
+            System.out.println("Error: No s'ha trobat cap personatge amb el nom '" + nom +"'");
+        }
+    }
+
+    // Funcion filtrar
+    public static void filtrar(String columna, String valor, ArrayList<HashMap<String, Object>> tabla) {
+        boolean encontrado = false;
+    
+        String columnaNormalizada = capitalize(columna);
+    
+        System.out.println("Resultat del filtre: '" + columna + "' = '" + valor + "':");
+        for (HashMap<String, Object> fila : tabla) {
+            Object columnaValorObj = fila.get(columnaNormalizada);
+            if (columnaValorObj != null) {
+                String columnaValor = columnaValorObj.toString();
+                if (columnaValor.equalsIgnoreCase(valor)) {
+                    System.out.println(fila);
+                    encontrado = true;
+                }
+            }
+        }
+    
+        if (!encontrado) {
+            System.out.println("No s'ha trobat cap registre que coincideixi amb el filtre.");
+        }
+    }
+    
+
+    // Crear una id entre el 10 i 99 para las personas
+    public static void generaId(ArrayList<HashMap<String, Object>> tabla) {
+        Set<Integer> idsGenerados = new HashSet<>();
+
+        for(HashMap<String, Object> fila : tabla) {
+            int id;
+            do { 
+                id = (int) (Math.random() * 90) + 10; // Genera un ID entre 10 y 99
+            } while (idsGenerados.contains(id));
+
+            idsGenerados.add(id);
+            fila.put("ID" , id);
+         }
+    }
+
+    // ORDENAR POR NOMBRE ALFABETICAMENTE
+    public static void mostrarTablaOrdenadaNombre(ArrayList<HashMap<String, Object>> tabla) {
+        tabla.sort((fila1, fila2) -> { 
+            String valor1 = (String) fila1.get("Nom");
+            String valor2 = (String) fila2.get("Nom");
+            return valor1.compareTo(valor2);
+        });
+    }
+
+    // ORDENAR POR PAIS ALFABETICAMENTE
+    public static void mostrarTablaOrdenadaPais(ArrayList<HashMap<String, Object>> tabla) {
+        tabla.sort((fila1, fila2) -> { 
+            String valor1 = (String) fila1.get("Pais");
+            String valor2 = (String) fila2.get("Pais");
+            return valor1.compareTo(valor2);
+        });
+    }
+
+    //ORDENAR POR DISCIPLINA ALFABETICAMENTE
+    public static void mostrarTablaOrdenadaDisciplina(ArrayList<HashMap<String, Object>> tabla) {
+        tabla.sort((fila1, fila2) -> { 
+            String valor1 = (String) fila1.get("Disciplina");
+            String valor2 = (String) fila2.get("Disciplina");
+            return valor1.compareTo(valor2);
+        });
+    }
+
+    // ORDENAR POR NAIXEMENT NUMERICAMENTE
+    public static void mostrarTablaOrdenadaNaixement(ArrayList<HashMap<String,Object>> tabla) {
+        tabla.sort((fila1, fila2) -> {
+            Integer valor1 = (Integer) fila1.get("Naixement");
+            Integer valor2 = (Integer) fila2.get("Naixement");
+            return valor1.compareTo(valor2);
+        });
+    }
+
+    // ORDENAR POR NOMBRE INVERSION 
+    public static void mostrarTablaOrdenadaNomInvertido(ArrayList<HashMap<String,Object>> tabla) {
+        tabla.sort((fila1, fila2) -> {
+            String valor1 = (String) fila1.get("Nom");
+            String valor2 = (String) fila2.get("Nom");
+
+            return valor2.compareTo(valor1);
+        });
+    }
+
+    // ORDENAR POR PAIS INVERSION 
+    public static void mostrarTablaOrdenadaPaisInvertido(ArrayList<HashMap<String,Object>> tabla) {
+        tabla.sort((fila1, fila2) -> {
+            String valor1 = (String) fila1.get("Pais");
+            String valor2 = (String) fila2.get("Pais");
+
+            return valor2.compareTo(valor1);
+        });
+    }
+
+    // ORDENAR POR DISCIPLINA INVERSION 
+    public static void mostrarTablaOrdenadaDisciplinaInvertido(ArrayList<HashMap<String,Object>> tabla) {
+        tabla.sort((fila1, fila2) -> {
+            String valor1 = (String) fila1.get("Disciplina");
+            String valor2 = (String) fila2.get("Disciplina");
+
+            return valor2.compareTo(valor1);
+        });
+    }
+
+    // ORDENAR POR NAIXEMENT INVERSION 
+    public static void mostrarTablaOrdenadaNaixementInvertido(ArrayList<HashMap<String,Object>> tabla) {
+        tabla.sort((fila1, fila2) -> {
+            Integer valor1 = (Integer) fila1.get("Naixement");
+            Integer valor2 = (Integer) fila2.get("Naixement");
+
+            return valor2.compareTo(valor1);
+        });
+    }
+
 }
+
